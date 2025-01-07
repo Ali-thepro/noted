1701c08c1bfe08364d1f2a673485d43a05cb452e
Kaushal Sambhe <kaushal.sambhe2@mail.dcu.ie>
2025-01-07 15:08:18 +0000

    KAN-3: Set up mongodb with backend

--- code/backend/app.js
+++ code/backend/app.js
@@ -2,9 +2,21 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 const config = require('./utils/config');
+const logger = require('./utils/logger');
 
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(request => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
--- code/backend/utils/config.js
+++ code/backend/utils/config.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
+const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
 const UI_URI = process.env.UI_URI;
 const SERVER_URI = process.env.SERVER_URI;
 
 module.exports = {
+  MONGODB_URI,
   PORT,
   UI_URI,
   SERVER_URI
