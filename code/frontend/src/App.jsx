87d00ad3d5590a63fc1c0d3e526f5616cc083abc
Kaushal Sambhe <kaushal.sambhe2@mail.dcu.ie>
2025-01-08 16:52:49 +0000

    KAN-12: Devoloped Scrolling feature, About and Not found page

--- code/frontend/src/App.jsx
+++ code/frontend/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import ScrollToTop from './components/ScrollToTop'
+import NotFound from './pages/NotFound'
 import Header from './components/Header'
+import About from './pages/About'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -9,6 +12,7 @@ const App = () => {
 
   return (
     <Router>
+      <ScrollToTop />
       <Header />
       <ToastContainer
         position="top-right"
@@ -19,6 +23,8 @@ const App = () => {
       />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   )
