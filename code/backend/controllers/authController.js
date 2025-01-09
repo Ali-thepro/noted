e8130139ac63e35042c56604049d378c1a62ea35
Kaushal Sambhe <kaushal.sambhe2@mail.dcu.ie>
2025-01-09 15:13:55 +0000

    KAN-17: Changed backend routes to handle cli login

--- code/backend/controllers/authController.js
+++ code/backend/controllers/authController.js
@@ -152,7 +152,6 @@ const google = async (request, response, next) => {
         code,
       },
     })
-    
     const { id_token, access_token } = data
 
     const { data: userProfile } = await axios({
