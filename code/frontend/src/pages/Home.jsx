ac01155f8fbe7baf852e7289d7f94c77d7d6f323
ahmada5 <ali.ahmad5@mail.dcu.ie>
2025-01-09 12:44:42 +0000

    KAN-16: Fixed missing UseEffect dependency

--- code/frontend/src/pages/Home.jsx
+++ code/frontend/src/pages/Home.jsx
@@ -1,26 +1,26 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { me } from '../services/auth';
-import { setUser } from '../redux/reducers/authReducer';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import { me } from '../services/auth'
+import { setUser } from '../redux/reducers/authReducer'
+import { useEffect } from 'react'
 import { toast } from 'react-toastify'
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const user = useSelector(state => state.auth.user);
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.auth.user)
   useEffect(() => {
     async function fetchUser() {
       try {
         if (!user) {
-          const response = await me(); 
-          dispatch(setUser(response));
-          toast.success('Logged in');
+          const response = await me()
+          dispatch(setUser(response))
+          toast.success('Logged in')
         }
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
     }
-    fetchUser();
-  }, [dispatch]);
+    fetchUser()
+  }, [dispatch, user])
   return (
     <div className="container mx-auto p-4">
       <h1 className='text-3xl'>Welcome to the Home Page</h1>
