e85c55de6a33b9738c68860a07ef2b278e96cc24
ahmada5 <ali.ahmad5@mail.dcu.ie>
2025-01-16 14:03:00 +0000

    KAN-28: Added tooltips to toolbar and added imag upload service

--- code/frontend/src/components/Editor/Toolbar.jsx
+++ code/frontend/src/components/Editor/Toolbar.jsx
@@ -1,4 +1,4 @@
-import { Button, ButtonGroup } from 'flowbite-react'
+import { Button, Tooltip } from 'flowbite-react'
 import {
   FaBold, FaItalic, FaLink, FaImage,
   FaListUl, FaListOl, FaCode, FaQuoteLeft,
@@ -6,6 +6,24 @@ import {
   FaTable, FaMinus, FaComment, FaUpload
 } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+
+const toolbarItems = [
+  { action: 'header', icon: <FaHeading />, tooltip: 'Add Heading', id: 'header-button' },
+  { action: 'bold', icon: <FaBold />, tooltip: 'Bold Text', id: 'bold-button' },
+  { action: 'italic', icon: <FaItalic />, tooltip: 'Italic Text', id: 'italic-button' },
+  { action: 'strikethrough', icon: <FaStrikethrough />, tooltip: 'Strikethrough Text', id: 'strikethrough-button' },
+  { action: 'link', icon: <FaLink />, tooltip: 'Insert Link', id: 'link-button' },
+  { action: 'image', icon: <FaImage />, tooltip: 'Upload Image', id: 'image-button' },
+  { action: 'upload', icon: <FaUpload />, tooltip: 'Upload File', id: 'upload-button' },
+  { action: 'ul', icon: <FaListUl />, tooltip: 'Unordered List', id: 'ul-button' },
+  { action: 'ol', icon: <FaListOl />, tooltip: 'Ordered List', id: 'ol-button' },
+  { action: 'checklist', icon: <FaCheckSquare />, tooltip: 'Checklist', id: 'checklist-button' },
+  { action: 'code', icon: <FaCode />, tooltip: 'Code Block', id: 'code-button' },
+  { action: 'quote', icon: <FaQuoteLeft />, tooltip: 'Quote Block', id: 'quote-button' },
+  { action: 'table', icon: <FaTable />, tooltip: 'Insert Table', id: 'table-button' },
+  { action: 'line', icon: <FaMinus />, tooltip: 'Horizontal Line', id: 'line-button' },
+  { action: 'comment', icon: <FaComment />, tooltip: 'Add Comment', id: 'comment-button' }
+]
 
 const Toolbar = ({ onAction }) => {
   const handleMarkdownAction = (action) => {
@@ -15,7 +33,8 @@ const Toolbar = ({ onAction }) => {
       italic: { prefix: '_', suffix: '_' },
       strikethrough: { prefix: '~~', suffix: '~~' },
       link: { prefix: '[', suffix: '](url)' },
-      image: { prefix: '![', suffix: '](url)', custom: true },
+      image: { prefix: '![', suffix: '](url)' },
+      upload: { prefix: '![', suffix: '](url)' },
       ul: { prefix: '- ', suffix: '' },
       ol: { prefix: '1. ', suffix: '' },
       checklist: { prefix: '- [ ] ', suffix: '' },
@@ -31,53 +50,32 @@ const Toolbar = ({ onAction }) => {
 
   return (
     <div className="p-2 border-b dark:border-gray-700 bg-white dark:bg-gray-800">
-      <ButtonGroup>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('header')}>
-          <FaHeading />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('bold')}>
-          <FaBold />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('italic')}>
-          <FaItalic />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('strikethrough')}>
-          <FaStrikethrough />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('link')}>
-          <FaLink />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('image')}>
-          <FaImage />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('upload')}>
-          <FaUpload />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('ul')}>
-          <FaListUl />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('ol')}>
-          <FaListOl />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('checklist')}>
-          <FaCheckSquare />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('code')}>
-          <FaCode />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('quote')}>
-          <FaQuoteLeft />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('table')}>
-          <FaTable />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('line')}>
-          <FaMinus />
-        </Button>
-        <Button color="gray" size="sm" onClick={() => handleMarkdownAction('comment')}>
-          <FaComment />
-        </Button>
-      </ButtonGroup>
+      <div className="overflow-x-auto">
+        <div className="inline-flex min-w-max">
+          {toolbarItems.map((item, index) => (
+            <Tooltip 
+              key={item.action}
+              content={item.tooltip}
+            >
+              <Button
+                color="gray"
+                size="sm"
+                onClick={() => handleMarkdownAction(item.action)}
+                className={`
+                  focus:ring-0
+                  ${index === 0 ? 'rounded-l-lg rounded-r-none' : ''}
+                  ${index === toolbarItems.length - 1 ? 'rounded-r-lg rounded-l-none' : ''}
+                  ${index !== 0 && index !== toolbarItems.length - 1 ? 'rounded-none' : ''}
+                  ${index !== 0 ? '-ml-px' : ''}
+                `}
+                id={item.id}
+              >
+                {item.icon}
+              </Button>
+            </Tooltip>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
