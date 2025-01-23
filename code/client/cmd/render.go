4fd93ab2fd682e06b645a7312e2653d016757f24
ahmada5 <ali.ahmad5@mail.dcu.ie>
2025-01-23 16:54:50 +0000

    KAN-38: Added command to preview a note using a markdown renderer

---
+++ code/client/cmd/render.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/charmbracelet/glamour"
+	"github.com/spf13/cobra"
+	"noted/internal/storage"
+)
+
+var previewCmd = &cobra.Command{
+	Use:   "preview [id]",
+	Short: "Preview a note with markdown formatting",
+	Long: `Preview a note's content with markdown formatting using glamour.
+You can specify either the note ID/shortID as an argument or use --title flag.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		var noteToPreview *storage.Note
+		var err error
+
+		if len(args) > 0 {
+			noteToPreview, err = storage.GetNoteByID(args[0])
+			if err != nil {
+				return err
+			}
+		} else {
+			title, err := cmd.Flags().GetString("title")
+			if err != nil {
+				return err
+			}
+			if title == "" {
+				return fmt.Errorf("either provide a note ID as an argument or use --title flag")
+			}
+
+			noteToPreview, err = storage.SelectNote(title)
+			if err != nil {
+				return err
+			}
+		}
+
+		content, err := storage.ReadNoteContent(noteToPreview.Filename)
+		if err != nil {
+			return fmt.Errorf("failed to read note content: %w", err)
+		}
+
+		renderer, err := glamour.NewTermRenderer(
+			glamour.WithAutoStyle(),
+			glamour.WithWordWrap(80),
+		)
+		if err != nil {
+			return fmt.Errorf("failed to create markdown renderer: %w", err)
+		}
+
+		rendered, err := renderer.Render(content)
+		if err != nil {
+			return fmt.Errorf("failed to render markdown: %w", err)
+		}
+
+		fmt.Print(rendered)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(previewCmd)
+	previewCmd.Flags().StringP("title", "t", "", "Title of the note to render")
+}
