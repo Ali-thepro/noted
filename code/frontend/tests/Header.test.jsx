8a7ab8a31a221c58cf86c3da1d45dd34ce9cfd84
ahmada5 <ali.ahmad5@mail.dcu.ie>
2025-01-26 00:34:29 +0000

    KAN-41: Added sign in page tests

--- code/frontend/tests/Header.test.jsx
+++ code/frontend/tests/Header.test.jsx
@@ -1,9 +1,9 @@
 import { screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { render } from './test-utils'
+import Header from '../src/components/Header'
 import { http, HttpResponse } from 'msw'
 import { setupServer } from 'msw/node'
-import Header from '../src/components/Header'
 
 const server = setupServer(
   http.post('/api/auth/signout', () => {
