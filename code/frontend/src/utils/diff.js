78815b5f55d6890cc3e8958c3890e4152f5e0788
ahmada5 <ali.ahmad5@mail.dcu.ie>
2025-02-01 00:12:45 +0000

    KAN-43: Finished functionality to allow conversion of diffs to delta and reversion of deltas to diffs

--- code/frontend/src/utils/diff.js
+++ code/frontend/src/utils/diff.js
@@ -1,12 +1,30 @@
 import { diff_match_patch } from 'diff-match-patch'
 
 function encode(str) {
-  return encodeURIComponent(str)
-    .replace(/[!'()*]/g, function(c) {
-      return '%' + c.charCodeAt(0).toString(16).toUpperCase()
-    })
-    .replace(/%20/g, '+')
+  return encodeURIComponent(str.replace(/%20/g, '+'))
 }
+
+function unescaper(delta) {
+  return delta
+    .replace(/%21/g, '!')
+    .replace(/%7E/g, '~')
+    .replace(/%27/g, '\'')
+    .replace(/%28/g, '(')
+    .replace(/%29/g, ')')
+    .replace(/%3B/g, ';')
+    .replace(/%2F/g, '/')
+    .replace(/%3F/g, '?')
+    .replace(/%3A/g, ':')
+    .replace(/%40/g, '@')
+    .replace(/%26/g, '&')
+    .replace(/%3D/g, '=')
+    .replace(/%2B/g, '+')
+    .replace(/%24/g, '$')
+    .replace(/%2C/g, ',')
+    .replace(/%23/g, '#')
+    .replace(/%2A/g, '*')
+}
+
 
 
 diff_match_patch.prototype.diff_toDelta = function (diffs) {
@@ -31,11 +49,53 @@ diff_match_patch.prototype.diff_toDelta = function (diffs) {
     }
   }
 
-  return delta.join('\t')
+  const deltaParts = delta.join('\t')
+  return unescaper(deltaParts)
 }
 
-// diff_match_patch.prototype.diff_fromDelta = function (text, delta) {
+diff_match_patch.prototype.diff_fromDelta = function (text, delta) {
+  let diffs = []
+  let i = 0
 
-// }
+  const runes = [...text]
+  const tokens = delta.split('\t')
+
+  for (let token of tokens) {
+    if (token.length === 0) {
+      continue
+    }
+
+    const op = token.charAt(0)
+    let param = token.charAt(1)
+
+    if (op === '+') {
+      param = param.replace(/\+/g, '%2b')
+      param = decodeURIComponent(param)
+      diffs.push([diff_match_patch.DIFF_INSERT, param])
+    } else if (op === '-' || op === '=') {
+      const n = parseInt(param, 10)
+      if (isNaN(n) || n < 0) {
+        throw new Error('Invalid number in DiffFromDelta:' + param)
+      }
+      i += n
+      if (i > runes.length) {
+        break
+      }
+      const text = runes.slice(i - n, i).join('')
+      if (op === '-') {
+        diffs.push([diff_match_patch.DIFF_DELETE, text])
+      } else {
+        diffs.push([diff_match_patch.DIFF_EQUAL, text])
+      }
+    } else {
+      throw new Error('Unknown DiffOp: ' + op)
+    }
+  }
+  if (i !== runes.length) {
+    throw new Error('Delta length larger than source text')
+  }
+
+  return diffs
+}
 
 export default diff_match_patch
