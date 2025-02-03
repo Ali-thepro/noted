34a0fd095f366c272c6e33625e84b66e276af787
ahmada5 <ali.ahmad5@mail.dcu.ie>
2025-02-03 00:28:40 +0000

    KAN-44: Added new test cases for signin

--- code/backend/tests/test_helper.js
+++ code/backend/tests/test_helper.js
@@ -28,59 +28,10 @@ const getUsersInDb = async () => {
   return users.map(user => user.toJSON())
 }
 
-const getAuthCookie = async () => {
-  await api
-    .post('/api/auth/signup')
-    .send(initialUsers[0])
-
-  const response = await api
-    .post('/api/auth/signin')
-    .send({
-      email: initialUsers[0].email,
-      password: initialUsers[0].password
-    })
-
-  const cookies = response.headers['set-cookie']
-  return cookies.join('; ')
-}
-
-const createTestUser = async (userData = initialUsers[0]) => {
-  const response = await api
-    .post('/api/auth/signup')
-    .send(userData)
-
-  return response.body
-}
-
-const createTestNote = async (noteData, authCookie) => {
-  const response = await api
-    .post('/api/notes')
-    .set('Cookie', authCookie)
-    .send(noteData)
-
-  return response.body
-}
-
-const mockGoogleProfile = {
-  email: 'google@test.com',
-  name: 'Google User',
-  id: '123456789'
-}
-
-const mockGithubProfile = {
-  email: 'github@test.com',
-  login: 'githubuser',
-  id: '987654321'
-}
 
 module.exports = {
   api,
   initialUsers,
   clearDatabase,
   getUsersInDb,
-  getAuthCookie,
-  createTestUser,
-  createTestNote,
-  mockGoogleProfile,
-  mockGithubProfile
 }
