144bf62268afcac9cdb1848e0bb0bdaaf09ba62a
Kaushal Sambhe <kaushal.sambhe2@mail.dcu.ie>
2025-02-11 15:56:37 +0000

    KAN-41 Add start:test script to package.json for running the server in test environment

--- code/playwright/tests/signup.spec.js
+++ code/playwright/tests/signup.spec.js
@@ -12,12 +12,11 @@ test.describe('User Registration', () => {
 
     const username = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
     await page.getByPlaceholder('Username').fill(username);
-    await page.getByPlaceholder('name@company.com').fill(username + '@example.com');
-    await page.getByPlaceholder('Password').first().fill('password123');
-    await page.getByPlaceholder('Confirm password').fill('password123');
+    await page.getByPlaceholder('name@company.com').fill(username + '@example.com')
+    await page.getByPlaceholder('Password').first().fill('password123')
+    await page.getByPlaceholder('Confirm password').fill('password123')
 
     await page.getByRole('button', { name: 'Sign up' }).click()
     await expect(page).toHaveURL('http://localhost:5173/signup')
   })
-  
 })
